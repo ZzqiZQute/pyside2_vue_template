11a2b34bfdebfba5455baebc1789f51556ce651e
ZzqiZQute <zhaozq11th@ustbsmartcar.org>
2019-10-18 12:24:54 +0800

    modify .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,16 @@
-module.exports={
-    rules:{
-        "no-console":process.env.NODE_ENV==='production'?'error':'off',
-        "no-undef":process.env.NODE_ENV==='production'?'error':'off',
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: 'module'
+    },
+    parser: "vue-eslint-parser",
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        "no-undef": process.env.NODE_ENV === 'production' ? 'error' : 'off',
     }
 }
